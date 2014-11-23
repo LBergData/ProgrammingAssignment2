5b25ad907ce4385bb5d6d92df7c0f7557e8518eb
Laurence Berg <laurence.berg@gmail.com>
2014-11-23 16:19:57 -0500

    First version of programming assignment 1 by LBerg

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,73 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions are being submitted:
+##	makeCacheMatrix
+##		This function prepares the environment for the cacheSolve
+##		 function to operate. 
+##		It holds a matrix vector and sets up additional 
+##		sub-functions (methods) that can be called by the separate 
+##		function.
+##		It's main purpose is to allow for holding a function result
+##		 in memory in case it needs to be reused.
+##		This allows a calling function to skip re-running an operation 
+##		if it's been run before with the same input.
+##
+##	cacheSolve 
+##		This function has as its basic task to invert a given matrix.
+##		However, it also makes use of the makeCacheMatrix function in order to avoid re-doing
+##		the matrix inversion if it has already been done for the same matrix.
 
-## Write a short comment describing this function
+
+## This function sets up sub-functions (methods) that can be called
+## by an external function (see internal comments).
+## It also holds a matrix vector that is passed to it for future use.
+## Designed to work with cacheSolve function.
 
 makeCacheMatrix <- function(x = matrix()) {
+
+
+		# reset output vector to NULL every time makeCacheMatrix is called
+InvertMatrix <- NULL    
+
+		# create internal function to store variables
+set <- function(y) {
+   	    x <<- y
+       	    InvertMatrix <<- NULL
+       		 }
+
+		# create internal function (method) available to external function call 
+		# will return value of vector supplied to makeCacheMatrix
+get <- function() { x } 
+
+		# create internal function (method) available to external function call 
+		# will store the value passed to it into output vector (using "superassignment")
+setInvert <- function(Invert)  { InvertMatrix <<- Invert }
+
+		# internal function (method) available to external function call
+		# will return the cached value of output vecotr to calling function
+getInvert <- function() { InvertMatrix }
+
+		# set up labels for external function to use
+    list(get = get, set = set,               
+         setInvert = setInvert,  
+         getInvert = getInvert)  
 
 }
 
 
-## Write a short comment describing this function
+## Returns the inversion of a square matrix (x).
+## Is designed to work with makeCacheMatrix function which
+## allows this function to check first if the matrix inversion
+## is already available in memory, and, if so, returns the results
+## from memory instead of recalculating the inversion
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        Invert <- x$getInvert()
+        if(!is.null(Invert)) {
+                message("getting cached data")
+                return(Invert)
+        }
+        data <- x$get()
+        Invert <- solve(data, ...)
+        x$setInvert(Invert)
+        Invert
+
 }
